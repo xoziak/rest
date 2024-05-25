e8bc272413f63ca6ed49e379ca97b5201cb16d03
xoziak <xoziak@gmail.com>
2024-05-25 22:35:42 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Database connection configuration
-const config = {
-	user: 'postgres',
-	password: '123456',
-	host: 'localhost',
-	port: '5432',
-	database: 'postgres',
-};
-module.exports = config;
